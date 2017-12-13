0e24a194dad77498bcb1a6549228f8549d1cdb51
patricio <saersito123@gmail.com>
2017-12-12 22:31:17 -0600

    operaciones matematicas

--- holamundo.cpp
+++ holamundo.cpp
@@ -1,8 +1,18 @@
 #include<iostream>
 using namespace std;
-int main (){
-	cout<<"hola mundo";
-	 system("pause");
-	return 0;
-	
-}
+int main ()
+{
+	int a,b,suma=0,resta=0,multiplicacion=0,divicion=0;
+	cout<<"digita el primer numero \n"; cin>>a;
+	cout<<"digita el segundo numero \n"; cin>>b;
+	suma=a+b;
+	resta=a-b;
+	multiplicacion=a*b;
+	divicion=a/b;
+	cout<<"la suma es"<<suma<<endl;
+	cout<<"la resta es "<<resta<<endl;
+	cout<<"la multiplicacion es"<<multiplicacion<<endl;
+	cout<<"la divicion es "<<divicion<<endl;
+
+system("pause");
+return 0;	}
