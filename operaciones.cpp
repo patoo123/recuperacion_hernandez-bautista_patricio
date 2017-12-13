ae878473f528b28719442fe06003037f4697637e
patricio <saersito123@gmail.com>
2017-12-12 22:27:38 -0600

    operaciones

---
+++ operaciones.cpp
@@ -0,0 +1,19 @@
+//operaciones suma,resta,multiplicacion,divicion
+#include<iostream>
+using namespace std;
+int main ()
+{
+	int a,b,suma=0,resta=0,multiplicacion=0,divicion=0;
+	cout<<"digita el primer numero \n"; cin>>a;
+	cout<<"digita el segundo numero \n"; cin>>b;
+	suma=a+b;
+	resta=a-b;
+	multiplicacion=a*b;
+	divicion=a/b;
+	cout<<"la suma es"<<suma<<endl;
+	cout<<"la resta es "<<resta<<endl;
+	cout<<"la multiplicacion es"<<multiplicacion<<endl;
+	cout<<"la divicion es "<<divicion<<endl;
+
+system("pause");
+return 0;	}
